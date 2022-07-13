ea3bb74ee5534fca38f458a2dcc9782035652553
Dong-Hyeok-Ing <kdhhdk83@gmail.com>
2022-07-14 03:04:42 +0900

    move img

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <nav>
+    <h1 class="head_logo"><router-link to="/"><img alt="부팅로고" src="./assets/img/logo_s.png"></router-link></h1> |
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view></router-view>
 </template>
 
 <style>
+.head_logo{}
+.head_logo img{width: 140px;}
+.btn-common-views{display:inline-block;vertical-align:middle;height:59px;line-height:57px;text-align:center;border:1px solid #333;font-size:17px;color:#333;padding:0 19px 0 23px;transition:all .3s ease}
+.humans .photo img{padding: 10px}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
